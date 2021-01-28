c54bcb5e7437220a5150e1e1a431b3cf5fc08bc5
Auliya Michelle <48114085+Auliya123@users.noreply.github.com>
2021-01-28 16:52:42 +0700

    emotion css todos component

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import Todo from "../todo/Todo";
 import PropTypes from "prop-types";
 
-import styles from "./todos.module.css";
+// import styles from "./todos.module.css";
+import * as styles from "./todos.styles";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section className={styles.todos}>
+    <section css={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -20,10 +22,10 @@ const Todos = ({ todos, completeTodo }) => {
           );
         })}
       {todos.length === 0 && (
-        <div className={styles.todoPlaceholderText}>
+        <div css={styles.todoPlaceholderText}>
           No todos yet. Add todo by clicking{" "}
-          <span className={styles.addButtonPlaceholderText}>ADD</span> button on
-          top left corner.
+          <span css={styles.addButtonPlaceholderText}>ADD</span> button on top
+          left corner.
         </div>
       )}
     </section>
